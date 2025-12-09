34cb0d5224a569c704643d8193e374a88781e7e6
Aneta Mertová <anetamertova@ydWLS-U1-12-100.uibk.ac.at>
2025-12-09 14:46:02 +0100

    Add random noise seed and implement Numba backend

--- src/rdsolver/model/initial_conditions.py
+++ src/rdsolver/model/initial_conditions.py
@@ -1,17 +1,24 @@
 import numpy as np
 
-def create_initial_conditions(size: int) -> tuple[np.ndarray, np.ndarray]:
+def create_initial_conditions(size: int, noise_seed: int = None) -> tuple[np.ndarray, np.ndarray]:
     """
-    Create initial field for gray scott with a single v spot in the center.
+    Create initial field for Gray-Scott with a single v spot in the center,
+    plus optional random noise.
     """
+    if noise_seed is not None:
+        np.random.seed(noise_seed)
 
     u_conc = np.ones((size, size), dtype=float)
     v_conc = np.zeros((size, size), dtype=float)
 
-    r = size // 10 # Size of the spot
-    c = size // 2 # center of the grid
+    r = size // 10  # Size of the spot
+    c = size // 2   # Center of the grid
 
     u_conc[c-r:c+r, c-r:c+r] = 0.50
     v_conc[c-r:c+r, c-r:c+r] = 0.25
+
+    # Add small random noise
+    u_conc += 0.02 * np.random.rand(size, size)
+    v_conc += 0.02 * np.random.rand(size, size)
 
     return u_conc, v_conc
--- src/rdsolver/solvers/euler_numba.py
+++ src/rdsolver/solvers/euler_numba.py
@@ -1,4 +1,19 @@
+from numba import njit
+import numpy as np
 from src.rdsolver.solvers.euler_base import ExplicitEulerBase
+
+@njit
+def laplacian_numba(conc):
+    size_x, size_y = conc.shape
+    lap = np.zeros_like(conc)
+    for i in range(size_x):
+        for j in range(size_y):
+            up = conc[i-1, j] if i > 0 else conc[i, j]
+            down = conc[i+1, j] if i < size_x-1 else conc[i, j]
+            left = conc[i, j-1] if j > 0 else conc[i, j]
+            right = conc[i, j+1] if j < size_y-1 else conc[i, j]
+            lap[i, j] = up + down + left + right - 4.0 * conc[i, j]
+    return lap
 
 class ExplicitEulerNumba(ExplicitEulerBase):
     """
@@ -6,4 +21,5 @@ class ExplicitEulerNumba(ExplicitEulerBase):
     """
 
     def laplacian(self, conc):
-        pass
+        conc = np.asarray(conc, dtype=float)
+        return laplacian_numba(conc)
